b0f19762e649150a978b35b0a68e3a0ca2889c37
Beaver <beavernotacat@gmail.com>
2023-12-31 16:03:25 +1000

    DB feature: verdicts to the solves

--- web/problems/models.py
+++ web/problems/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import choices
 
 from RATS.settings import SUPPORTED_LANGUAGES
 from profiles.models import Users
@@ -19,7 +18,17 @@ class Tests(models.Model):
 
 
 class Solves(models.Model):
+    verdicts = {
+        "ok": "Done",
+        "wa": "Wrong answer",
+        "tl": "Time limit",
+        "ml": "Memory limit",
+        "ce": "Compilation error",
+        "re": "Runtime error"
+    }
+    
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     problem = models.ForeignKey(Problems, on_delete=models.CASCADE)
     language = models.CharField(max_length=30, choices=SUPPORTED_LANGUAGES)
     code = models.TextField()
+    verdict = models.CharField(max_lenght=2, choices=verdicts)
